a697d5b7001e7270e93cb0e04ad3da32efb2cac5
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-10-23 11:07:24 +0000

    hellooooo

---
+++ task.py
@@ -0,0 +1,28 @@
+
+def Loves_me(number):
+    numbers1 = []
+    numbers = []
+    num1 = 0
+    while num1 != number:
+        num1 = num1 + 1
+        numbers1.append(num1)
+    for i in numbers1:
+        if i == number:
+            if i % 2 == 0:
+                i = "LOVES ME NOT"
+                numbers.append(i)
+            else:
+                i = "LOVES ME"
+                numbers.append(i)
+        else:
+            if i % 2 == 0:
+                i = "Loves me not"
+                numbers.append(i)
+            else:
+                i = "Loves me"
+                numbers.append(i)
+
+    return numbers
+
+
+
