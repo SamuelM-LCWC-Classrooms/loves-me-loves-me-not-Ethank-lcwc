acae20a91a8a730dbcd6c2856656aa200a7d5ac5
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-11-04 14:05:58 +0000

    suck it

--- task.py
+++ task.py
@@ -3,24 +3,27 @@ def loves_me(number):
     numbers1 = []
     numbers = str("Loves me,")
     num1 = 1
-    while num1 != number:
-        num1 = num1 + 1
-        numbers1.append(num1)
-    for i in numbers1:
-        if i == number:
-            if i % 2 == 0:
-                i = "LOVES ME NOT"
-                numbers = numbers + " " + i
+    if number == 1:
+        return numbers
+    else:
+        while num1 != number:
+            num1 = num1 + 1
+            numbers1.append(num1)
+        for i in numbers1:
+            if i == number:
+                if i % 2 == 0:
+                    i = "LOVES ME NOT"
+                    numbers = numbers + " " + i
+                else:
+                    i = "LOVES ME"
+                    numbers = numbers + " " + i
             else:
-                i = "LOVES ME"
-                numbers = numbers + " " + i
-        else:
-            if i % 2 == 0:
-                i = "Loves me not,"
-                numbers = numbers + " " + i
-            else:
-                i = "Loves me,"
-                numbers = numbers + " " + i
+                if i % 2 == 0:
+                    i = "Loves me not,"
+                    numbers = numbers + " " + i
+                else:
+                    i = "Loves me,"
+                    numbers = numbers + " " + i
     return numbers
 
 
